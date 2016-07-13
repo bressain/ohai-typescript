294250a33e343d73bd0381647f0a457d9fd6ffa3
Bressain Dinkelman <dinkelburt@gmail.com>
2016-07-13 16:36:43 -0600

    Add concept of an adhoc area

---
+++ services/adhoc/do-the-things.ts
@@ -0,0 +1,9 @@
+import * as repository from '../common/inf/potabos-repo'
+
+console.log('preparing to process all the potabos...')
+
+repository.getAllPotabos().forEach(x => {
+  console.log(`processing potabo: ${x.id}`)
+})
+
+console.log('done processing potabos')

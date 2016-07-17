0ad3d76cbd87211e34399b0dd066bbf1d9b933e9
Bressain Dinkelman <dinkelburt@gmail.com>
2016-07-16 20:43:02 -0600

    Make async/await actually work asynchronously

--- services/common/inf/potabos-repo.js
+++ services/common/inf/potabos-repo.js
@@ -1,9 +1,11 @@
 export function getAllPotabos () {
-  return [
-    { id: 1, potabo: 'peesah peezah', uri: 'http://www.potabo.com/pics/peesah.jpg' },
-    { id: 2, potabo: 'terp', uri: 'http://www.potabo.com/pics/1286335065187.png' },
-    { id: 3, potabo: 'hat dug', uri: 'http://www.potabo.com/pics/hatdug.jpg' }
-  ]
+  return new Promise(resolve => {
+    resolve([
+      { id: 1, potabo: 'peesah peezah', uri: 'http://www.potabo.com/pics/peesah.jpg' },
+      { id: 2, potabo: 'terp', uri: 'http://www.potabo.com/pics/1286335065187.png' },
+      { id: 3, potabo: 'hat dug', uri: 'http://www.potabo.com/pics/hatdug.jpg' }
+    ])
+  })
 }
 
 export function addPotabo (potabo) {
